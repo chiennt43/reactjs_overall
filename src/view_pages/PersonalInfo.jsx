d3758534c40e5cf1a5fa760d2c261560670ec7bf
Nguyen Thi Chien <chiennguyen040396@gmail.com>
2020-12-26 20:38:36 +0700

    chien commit lan 1

--- src/view_pages/PersonalInfo.jsx
+++ src/view_pages/PersonalInfo.jsx
@@ -1,6 +1,9 @@
 
 const PersonalInfo = () => {
-    return   <h1>Thông tin cá nhân</h1>
+    return   <h1>
+        Thông tin cá nhân
+        <p>commit lần 1</p>
+        </h1>
 }
 
 export default PersonalInfo
