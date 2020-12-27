784de725f7507e3c83e2ae2386475bf385f6b5a2
Nguyen Thi Chien <chiennguyen040396@gmail.com>
2020-12-27 19:42:42 +0700

    test3

--- src/view_pages/PersonalInfo.jsx
+++ src/view_pages/PersonalInfo.jsx
@@ -1,6 +1,8 @@
 
 const PersonalInfo = () => {
-    return   <h1>Thông tin cá nhân. test2</h1>
+    return   <h1>Thông tin cá nhân. test2
+        <p>test 3</p>
+    </h1>
 }
 
 export default PersonalInfo
